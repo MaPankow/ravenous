5fb032ab7c0e162ca112542d6009e6bbc2205829
MaPankow <162994783+MaPankow@users.noreply.github.com>
2025-01-09 16:34:03 +0100

    Array bei BusinessList hinzugefügt

--- src/Components/BusinessList.js
+++ src/Components/BusinessList.js
@@ -4,12 +4,14 @@ import Business from './Business';
 
 
 function BusinessList() {
+    const listItems= [
+        <Business key={1} />,
+        <Business key={2} />,
+        <Business key={3} />,
+        <Business key={4} />
+    ];
     return (
-        <div>
-            <Business />
-            <Business />
-            <Business />
-        </div>
+        <div>{listItems}</div>
     );
 }
 
