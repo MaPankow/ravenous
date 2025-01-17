1f6354bcfe10c0155eef2a949b2c052006a4aee2
MaPankow <162994783+MaPankow@users.noreply.github.com>
2025-01-17 17:25:41 +0100

    IDs und names in Search Bar geändert, um sie an die API anzupassen

--- src/Components/SearchBar.js
+++ src/Components/SearchBar.js
@@ -6,18 +6,18 @@ function SearchBar () {
         <div>
             <form action="">
                 <div>
-                    <label for="bestMatch">Best match</label>
-                    <input type="checkbox" id="bestMatch" />
-                    <label for="highestRated">Highes rated</label>
-                    <input type="checkbox" id="highestRated" />
-                    <label for="mostReviewed">Most reviewed</label>
-                    <input type="checkbox" id="mostReviewed" />
+                    <label htmlFor="best_match">Best match</label>
+                    <input type="radio" id="best_match" name="sort_by" />
+                    <label htmlFor="rating">Highes rated</label>
+                    <input type="radio" id="rating" name="sort_by" />
+                    <label htmlFor="review_count">Most reviewed</label>
+                    <input type="radio" id="review_count" name="sort_by" />
                 </div>
                 <div>
-                    <label for="searchBusiness">Search business</label>
-                    <input type="text" id="search" />
-                    <label for="city">City</label>
-                    <input type="text" id="city" />
+                    <label for="category">Search business</label>
+                    <input type="text" id="category" /><br />
+                    <label for="location">City</label>
+                    <input type="text" id="location" />
                 </div>
                 <div>
                     <input type="submit" value="Submit" />
