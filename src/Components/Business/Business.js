3dbda23ab5246e9c34277f4b0f1a97c9f5000f65
MaPankow <162994783+MaPankow@users.noreply.github.com>
2025-01-17 18:14:42 +0100

    Beispielcode für das Zusammenpiel Business.js und Business.css, noch ausbauen und bei den anderen Dateien implementieren

--- src/Components/Business/Business.js
+++ src/Components/Business/Business.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import './Business.css'; // css importieren und den divs classNames geben, die dann in css umgestylt werden können
 
 const business = {
     image:"/img/pexels-chan_walrus-941861.jpg",
@@ -14,15 +15,17 @@ const business = {
 
 function Business () {
     return (
-        <div>
-            <img src={business.image} alt="Foto of a table in restaurant Jocelyns Fine Kitchen" />
-            <h1>{business.name}</h1>
-            <div id="address">
+        <div className='business'> 
+            <img className='business-image' src={business.image} alt="Foto of a table in restaurant Jocelyns Fine Kitchen" />
+            <h2 className='business-name'>{business.name}</h2>
+            <div className='business-category'>
+                <p>Category: {business.category}</p>   
+            </div>
+            <div className="business-address">
                 <p>{business.address}</p>
                 <p>{business.zipcode} {business.city}</p>
             </div>
-            <div id="stats">
-                <p>Category: {business.category}</p>
+            <div className="business-statistics">
                 <p>Average rating: {business.rating}</p>
                 <p>Reviews: {business.rewiewCount}</p>
             </div>
